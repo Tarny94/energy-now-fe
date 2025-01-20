02546b8a1b62d389328fef7e8f86d88de3b170b9
Alexandru.Tarnicer <alexandru.tarnicer@theaccessgroup.com>
2025-01-20 16:04:54 +0200

    Made key enter work for loggin page & created client configuration page with the specific elements and styling part 2

--- energy-now/src/components/Input.tsx
+++ energy-now/src/components/Input.tsx
@@ -71,8 +71,7 @@ const InputComponent: React.FC<IInput> = ({onBlur, error = false, type, color, s
     endAdornment={endAdornment}
     error={error}
     onBlur={onBlur}
-
-    />;
+    />
 }
 
 export default InputComponent;
--- energy-now/src/pages/ClientConfiguration.tsx
+++ energy-now/src/pages/ClientConfiguration.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import InputComponent from "../components/Input";
-import { Tab } from "@mui/material";
 import TabButton from "../components/Button";
 import "./styles/ClientConfiguration.scss";
+import { AuthContext } from "../context/AuthContext";
+import MultipleSelectCheckmarks from "../components/MultipleSelectCheck";
+import { SPECIALIZATIONS } from "../utils/constants/Const";
 
 const ClientConfiguration: React.FC = () => {
+    const {error} = React.useContext(AuthContext);
+
+    const [selectedSpecialisations, setSelectedSpecialisations] = React.useState<string[]>([]);
+
+
   return <div className="client-configuration-container">
     <h1 className="client-configuration-title">CLIENT CONFIGURATION</h1>
     <p>Client Configuration Content</p>
@@ -18,7 +25,7 @@ const ClientConfiguration: React.FC = () => {
             />
             <InputComponent 
                 onChange={() => {}} 
-                placeholder="Client Name"
+                placeholder="Firm Name"
                 required
                 type="text"
                 width={350}
@@ -55,14 +62,17 @@ const ClientConfiguration: React.FC = () => {
             />
             <InputComponent 
                 onChange={() => {}} 
-                placeholder="Specialization"
-                width={350}
-            />
-            <InputComponent 
-                onChange={() => {}} 
                 placeholder="Details"
                 type="text"
                 width={350}
+            />
+                        <MultipleSelectCheckmarks               
+                  width={350} 
+                  setSelectedItems={setSelectedSpecialisations} 
+                  selectedItems={selectedSpecialisations}
+                  label="Specializations"
+                  items={SPECIALIZATIONS.length > 0 ? SPECIALIZATIONS : []}
+                  inputLabelName="Specializations"            
             />
         </div>
         <div className="client-configuration-buttons">
